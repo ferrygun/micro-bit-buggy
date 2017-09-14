b43c35e79205b0680a2d6fda51c0c6dd1012151d
Ferry Djaja <ferrygun@yahoo.com>
2017-09-14 11:48:50 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,6 @@
-var a = 1818;
+navigator.bluetooth.requestDevice({
+  filters: [{
+    namePrefix: 'BBC micro:bit',
+  }],
+  optionalServices: ['e95d93af-251d-470a-a062-fa1922dfa9a8'] //EVENTSERVICE_SERVICE_UUID
+});
